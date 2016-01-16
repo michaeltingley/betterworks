d1c2f3c806888180c58d3547592796f4f8927de9
Michael Tingley <michaelalantingley@gmail.com>
2016-01-15 22:57:50 -0800

    post_message is written but not tested

--- betterworks/chat/views.py
+++ betterworks/chat/views.py
@@ -2,11 +2,11 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
 
-from .models import Conversation, Participant
+from .models import Conversation, Message, Participant
 
 class AboutView(TemplateView):
     template_name = "chat/about.html"
@@ -70,15 +70,8 @@ def find_users(request):
 
 @login_required(login_url='/chat/login/')
 def get_messages(request):
-    shared_conversations = [
-        user_conversation
-        for user_conversation in request.user.participant.conversation_set.all()
-        if (set([participant.user.username for participant in user_conversation.participants.all()]) ==
-            set([request.user.username, request.POST['email']]))
-    ]
-    if len(shared_conversations) > 0:
-        shared_conversation = shared_conversations[0]
-    else:
+    shared_conversation = get_conversation_with_remote_email(request, request.POST['email'])
+    if not shared_conversation:
         # Create the new conversation
         shared_conversation = Conversation.objects.create()
         shared_conversation.participants.add(request.user.participant)
@@ -96,3 +89,34 @@ def get_messages(request):
             } for message in shared_conversation.message_set.all()
         ]
     })
+
+@login_required(login_url='/chat/login/')
+def post_message(request):
+    remote_email = request.POST['email']
+    shared_conversation = get_conversation_with_remote_email(request, remote_email)
+
+    if not shared_conversation:
+        return HttpResponseBadRequest("Conversation not specified or does not exist")
+
+    message_text = request.POST['message_text']
+
+    if not message_text:
+        return HttpResponseBadRequest("Must specify message")
+
+    shared_conversation.message_set.add(
+        participant = User.objects.get(username=remote_email),
+        text=message_text,
+    )
+
+
+def get_conversation_with_remote_email(request, remote_email):
+    shared_conversations = [
+        user_conversation
+        for user_conversation in request.user.participant.conversation_set.all().prefetch_related('participants')
+        if (set([participant.user.username for participant in user_conversation.participants.all()]) ==
+            set([request.user.username, remote_email]))
+    ]
+    if len(shared_conversations) > 0:
+        # TODO: This should never happen; once I've finalized the project, I should enforce this
+        return shared_conversations[0]
+    return None
